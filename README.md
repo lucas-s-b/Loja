a1bffb044ff9fb644ae8582e0ea3588dea71f136
lucas-s-b <lucasbarradascutrim@gmail.com>
2026-05-05 00:05:49 -0300

    sistema de disconto refeito, e readme melhorado

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Projeto Loja
 
+### Descrição
+
 Esse projeto é uma atividade para o curso de Fundamentos da Computação BICT UFMA.
 O mesmo faz uma simulação de um carrinho de compras em uma loja, é possivel escolher diferente opções de itens e até aplicar disconto em porcentagem, e ao final mostrar o total a ser pago.
 
@@ -9,5 +11,9 @@ instalar python
 botar no terminal
 
 ```shell
-main.py
+py main.py
 ```
+
+### Exemplo de uso:
+
+Ao dar Run no programa uma lista irá aparecer, para escolher uma opção é só escrever o número da opção mostrada na esquerda do texto e apertar Enter. Se for um produto ele será adcionado ao carrinho, a opção de adcionar disconto, irá pedir uma porcentagem quer será subtraída do total futuro, e a ultima mostra o total do carrinho e encerra o programa.
--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-def show_menu():
-    print("\n=== MENU DA LOJA ===")
-    print("1 - Maçã (5 R$)")
-    print("2 - Pão (10 R$)")
-    print("3 - Leite (8 R$)")
-    print("4 - Adcionar disconto.")
-    print("5 - Finalizar compra.")
-
 products = {
     1: ("Maçã", 5),
     2: ("Pão", 10),
@@ -17,36 +9,39 @@ discount = 0.0
 total = 0.0
 
 while True:
-    show_menu()
+    print("=== MENU DA LOJA ===")
+    print("1 - Maçã (5 R$)")
+    print("2 - Pão (10 R$)")
+    print("3 - Leite (8 R$)")
+    print("4 - Adcionar disconto.")
+    print("5 - Finalizar compra.\n")
     
     try:
         choice = int(input("Escolha uma opção: "))
     except:
-        print("Opção inválida! Utilize um número.")
+        print("Opção inválida! Utilize um número inteiro.\n")
         continue
 
     if choice == 4:
         try:
-            discount = float(input("Ponha o disconto em porcentagem: "))
+            discount = max(min(float(input("Ponha o disconto em porcentagem: ")), 100), 0)
+            print(f"Disconto atual: {discount}%.\n")
         except:
-            print("Disconto inválido! Utilize um número.")
-            continue
-    else:
+            print("Disconto inválido! Utilize um número.\n")
+        continue
+    elif choice == 5:
         break
 
     if choice in products:
         product_name, price = products[choice]
         cart.append(product_name)
         total += price
-        print(f"{product_name} adcionado ao carrinho.")
+        print(f"{product_name} adcionado ao carrinho.\n")
     else:
-        print("Opção inválida!")
+        print("Opção inválida!\n")
 
-if total > 100:
-    discount = total * 0.1
-    total -= discount
-
-print("\n=== SUMÁRIO DE COMPRA ===")
+print("\n=== SUMÁRIO DE COMPRA ===\n")
 print("Itens:", cart)
-print(f"Disconto: ${discount:.2f}")
-print(f"Total: ${total:.2f}")
+print(f"\nDisconto: {discount:.2f}%\n")
+print(f"Total sem disconto: {total:.2f} R$\n")
+print(f"Total: {total - (total * discount / 100):.2f} R$\n")
